e5c8a9d5b57d8f1d21cabe8edb0d9401909dd442
Sumon Hossain <106359888+Sumon180@users.noreply.github.com>
2023-01-11 11:04:29 +0600

    Add files via upload

---
+++ database-matching/models/userModel.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    require: true,
+  },
+  password: {
+    type: String,
+    require: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export default mongoose.model("user", userSchema);
